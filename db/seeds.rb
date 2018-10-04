e6af5195564e119ec001d91554136f4e79fef36f
danitiemi <danitiemi.br@gmail.com>
2018-10-04 11:54:59 -0700

    Rails api basic setup

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+ideas = Idea.create(
+  [
+    {
+      title: "Redesign Tweeter project",
+      body: "Using React and Rails"
+    },
+    {
+      title: "Next Vacation",
+      body: "Japan"
+    },
+    {
+      title: "Home office setup",
+      body: "Cool table with 2 monitors"
+    },
+    {
+      title: "My website",
+      body: "Design with some doodling and animation"
+    }
+  ])
